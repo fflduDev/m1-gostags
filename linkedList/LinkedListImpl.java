f62a3dcf959f25b371d52532f8f90abbab33e68f
Ryan Woodzell <ryanwoodzell@gmail.com>
2025-02-05 15:02:12 -0500

    aisdjli - !S1 :wq clear

--- linkedList/LinkedListImpl.java
+++ linkedList/LinkedListImpl.java
@@ -1,10 +1,12 @@
 package linkedList;
 
 public class LinkedListImpl implements LinkedList {
+	ListItem head;
 
 	@Override
 	public Boolean isItemInList(String thisItem) {
 		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
@@ -12,32 +14,101 @@ public class LinkedListImpl implements LinkedList {
 	public Boolean addItem(String thisItem) {
 		
 		
-		System.out.println("hello from addItem in LinkedListImpl - the item passed in: " + thisItem);
-	
-		return null;
+		//if head == null, nothing in the list
+				//create a new node with the data
+				//set that to head
+				// return
+
+				if(head == null) {
+					head = new ListItem(thisItem);
+					return true;
+					
+				}
+				
+				//else
+				//set head to local Node current
+				//step thru each element in the list, checking if the .next of this node is null
+				//once found, create a new node out of incoming data & set that to current.next
+				
+				ListItem current = head;
+				while(current.next != null) {
+					current = current.next;
+				}
+				current.next = new ListItem(thisItem);
+		return true;
 	}
 
 	@Override
 	public Integer itemCount() {
-		// TODO Auto-generated method stub
-		return null;
+		int count = 0;
+		ListItem tmp = head;
+		while (tmp != null) {
+			
+			count++;
+			tmp = tmp.next;
+	}
+		
+		return count;
 	}
 
 	@Override
 	public void listItems() {
-		// TODO Auto-generated method stub
-		
-	}
+		ListItem tmp = head;
+		while (tmp != null) {
+			
+			System.out.println(tmp.data);
+			tmp = tmp.next;
+	}}
 
 	@Override
 	public Boolean deleteItem(String thisItem) {
 		// TODO Auto-generated method stub
-		return null;
+		if(head == null)
+			return true;
+
+		//if head.data == incoming data
+		// set head = the node head points to.  return.
+		
+		//else
+		//create a new local node with the head
+		//step thru each element in the list.  if the next is non null
+		//check if that data == the data we want to delete
+		//if yes, this is the element to delete.  
+		//do that by setting current.next = current.next.next
+		//else, set current to the next element in the list
+		
+		ListItem current = head;
+		while(current.next != null) {
+			if(current.next.data == thisItem)
+				current.next = current.next.next;
+			else
+				current = current.next;
+		}
+		
+		return true;
 	}
 
 	@Override
 	public Boolean insertBefore(String newItem, String itemToInsertBefore) {
-		// TODO Auto-generated method stub
+		
+		if(head == null) {
+			addItem(newItem);
+			return true;
+		}
+		
+		ListItem current = head;
+		
+		if(current.data == head.data) {
+			addItem(head.data);
+			head.data = newItem;
+			return true;
+		}
+		
+		while(current.next != null) {
+			if (current.next.data == itemToInsertBefore) {
+				//TODO
+			}
+		}
 		return null;
 	}
 
@@ -52,5 +123,6 @@ public class LinkedListImpl implements LinkedList {
 		// TODO Auto-generated method stub
 		
 	}
+	
 	
 }
