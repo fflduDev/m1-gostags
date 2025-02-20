1e2a287bcca0ff3a6a75a2efc484680cdf786502
Ryan Woodzell <ryanwoodzell@gmail.com>
2025-02-20 15:25:14 -0500

    Impl

--- queue/QueueImpl.java
+++ queue/QueueImpl.java
@@ -1,40 +1,75 @@
 package queue;
 
 public class QueueImpl implements Queue {
-
+	String[] names = new String[6];
+	int front = -1;
+	int back = -1;
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+	     //first array: Index 0, last array index: 5. 
+	     if(back == 5) {
+	    	 return true;
+	     } else {
+	    	 return false;
+	     }
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		if(front == -1 && back == -1) {
+			return true;
+		}else {
+			return false;
+		}
+		
 	}
 
 	@Override
 	public void enQueue(String element) {
 		// TODO Auto-generated method stub
-		
+		if(isFull()) {
+			System.out.println("Queue is full");
+			
+		} else if (front == -1 && back ==-1) {
+			front = back = 0;
+			names[back] = element;
+		} else {
+			back++;
+			names[back] = element;
+		}
 	}
 
 	@Override
 	public String deQueue() {
-		// TODO Auto-generated method stub
+		if(isEmpty()) {
+			System.out.println("Queue is empty. Nothing to see here");
+			
+		} else if (front==back) {
+			front = back = -1;
+		} else {
+			front++;
+		}
 		return null;
 	}
 
 	@Override
 	public void display() {
-		// TODO Auto-generated method stub
 		
+		
+		if(isEmpty()) {
+			System.out.println("Queue is Empty");
+			
+		} else {
+			for (int i = front; i<= back; i++) {
+				System.out.println(names[i]);
+			}
+		}
 	}
 
 	@Override
 	public String peek() {
-		// TODO Auto-generated method stub
+		System.out.println("Front value is: " + names[front]);
 		return null;
 	}
 
--- queue/QueueTester.java
+++ queue/QueueTester.java
@@ -3,7 +3,15 @@ package queue;
 public class QueueTester {
 
 	public static void main(String[] args) {
-
+		Queue testQueue = new QueueImpl();
+		
+		testQueue.enQueue("Hello");
+		testQueue.enQueue("Whats up");
+		testQueue.enQueue("Howdy");
+		
+		testQueue.display();
+		
+		testQueue.peek();
 	}
 
 }
